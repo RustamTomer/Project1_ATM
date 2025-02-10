9cf0f5a25aedb965c4fd02f74e30ceec254bc765
Rustam Singh Tomer <91615721+RustamTomer@users.noreply.github.com>
2025-02-10 23:23:12 +0530

    Updated Project1_ATM.py

--- Project1_ATM.py
+++ Project1_ATM.py
@@ -1,55 +1,87 @@
-print("WELCOME TO EMZERR BANK")
-menu = ('Y')
-attempts=4
-balance=100000.00
-while attempts >= 0:
-    pin = int(input("Enter Your Pin :"))
-    if pin == (0000):
-        print("Pin entered by you is correct.")
-        while menu not in ('n', 'N', "No", "no"):
-            print("1 for Balance Enquiry")
-            print("2 For Cash Withdrawl")
-            print("3 to use EMZERR PAY")
-            print("4 to Return Card")
-            option=int(input("What would you like to chooose?"))
-            if option == 1:
-                print("Your Balance is Rs.", balance)
-                menu=input("Would you like to go back? (Y/N)")
-                if menu in ('n', 'N', "No", "no"):
-                    print("THANK YOU FOR USING EMZERR BANKING ATM")
-                    quit()
-            elif option == 2:
-                withdrawl = float(input("How much would you like to withdraw? Rs. 100/Rs. 500/Rs. 2000/Rs. 5000/Rs. 10000"))
-                if withdrawl in [100,500,2000,5000,10000]:
-                    balance = balance - withdrawl
-                    print("Reamaining Balance in your account is", balance)
-                    menu = input("Would you like to go back? (Y/N)")
-                    if menu in ('n', 'N', "No", "no"):
-                        print("THANK YOU FOR USING EMZERR BANKING ATM")
-                        quit()
-                elif withdrawl != [100,500,2000,5000,10000]:
-                    print("INVALID AMOUNT, RE-TRY")
-                    menu = ('Y')
-                elif withdrawl == 1:
-                    withdrawl=float(input("Enter Amount:"))
-            elif option == 3:
-                Pay = float(input("Enter amount to pe paid:"))
-                balance = balance - Pay
-                print("Reamaining Balance in your account is", balance)
-                menu = input("Would you like to go back? (Y/N)")
-                if menu in ('n', 'N', "No", "no"):
-                    print("THANK YOU FOR USING EMZERR BANKING SERVICES")
-                    quit()
-            elif option == 4:
-                print("Please wait returning in process")
-                print("THANK YOU FOR USING EMZERR BANKING SERVICES")
-                exit()
-            else:
-                print("Please enter a correct no.")
-                menu=('Y')
-    elif pin != (0000):
-        print("INCORRECT PIN")
-        attempts = attempts - 1
-        if attempts == 0:
-            print("Card Blocked")
-            break
+class Atm:
+
+    __counter = 1
+    def __init__(self):
+      self.__pin=""
+      self.__balance=0
+      self.s_no=Atm.__counter
+      Atm.__counter+=1
+
+      print(id(self))
+
+    @staticmethod
+    def get_counter():
+        return Atm.__counter
+
+    @staticmethod
+    def set_counter(new):
+        if type(new)==int:
+            Atm.__counter=new
+            print("Counter Changed")
+        else:
+            print("Not Allowed")
+      
+      #self.__menu()
+    def __menu(self):
+      user_input=input("""
+        Namaste! How would you like to proceed?
+        1. Enter 1 to create pin
+        2. Enter 2 to deposit
+        3. Enter 3 to withdraw
+        4. Enter 4 to check balance
+        5. Enter 5 to exit
+      """)
+      if user_input=="1":
+        self.create_pin()
+      elif user_input=="2":
+        self.deposit()
+      elif user_input=="3":
+        self.withdraw()
+      elif user_input=="4":
+        self.check_balance()
+      else:
+        print("Bye!")
+
+    def get_pin(self):
+      return self.__pin
+    
+    def set_pin(self,new_pin):
+      if type(new_pin)==str:
+        self.__pin=new_pin
+        print("Pin changed")
+      else:
+        print("Not allowed")
+
+    def create_pin(self):
+      self.__pin=(input("Enter the PIN: "))
+      print("PIN set successfully")
+      self.__menu()
+
+    def deposit(self):
+      temp = input("Enter the PIN: ")
+      if temp==self.__pin:
+                amount=int(input("Enter the amount: "))
+                self.__balance=self.__balance+amount
+                print("Deposit successful")
+      else:
+                print("Invalid PIN")
+      self.__menu()
+
+    def withdraw(self):
+      temp=input("Enter the PIN: ")
+      if temp==self.__pin:
+                amount=int(input("Enter the amount:"))
+                if amount < self.__balance:
+                  self.__balance=self.__balance-amount
+                  print("Withdraw successful")
+      else:
+                print("Invalid PIN")
+      self.__menu()
+
+    def check_balance(self):
+      temp=input("Enter the PIN: ")
+      if temp==self.__pin:
+                print(self.__balance)
+      else:
+                print("Invalid PIN")
+      self.__menu()
